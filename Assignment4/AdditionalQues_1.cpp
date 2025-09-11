63735bc0e72af262e7c421cbcaab41f34021a404
ArhanaMor1 <amor_be24@thapar.edu>
2025-09-11 06:01:34 +0530

    Update AdditionalQues_1.cpp

--- Assignment4/AdditionalQues_1.cpp
+++ Assignment4/AdditionalQues_1.cpp
@@ -1,25 +1,55 @@
 #include <iostream>
+using namespace std;
+
+string queue[100];
+int front=-1, rear=-1;
 int n;
-void binary();
+
+int isEmpty() {
+    if (front==-1 && rear==-1) return 1;
+    else return 0;
+}
+
+int isFull() {
+    if ((rear+1)%100==front) return 1;
+    else return 0;
+}
+
+void enqueue(string x) {
+    if (isFull()==0) {
+        if (isEmpty()==1) {
+            front=rear= 0;
+        } else {
+            rear=(rear+1)%100;
+        }
+        queue[rear]=x;
+    }
+}
+
+string dequeue() {
+    if (isEmpty()==1) return "";
+    string temp=queue[front];
+    if (front==rear) {
+        front=rear=-1;
+    } else {
+        front=(front + 1) % 100;
+    }
+    return temp;
+}
+
+void binary() {
+    enqueue("1");
+    for (int i=1; i<=n; i++) {
+        string current=dequeue();
+        cout<<current<<"\n";   
+        enqueue(current+"0");
+        enqueue(current+"1");
+    }
+}
+
 int main() {
-    std::cout<<"Enter n: ";
-    std::cin>>n;
+    cout<<"Enter n: ";
+    cin>>n;
     binary();
     return 0;
-}
-void binary() {
-    for(int i=1;i<= n;i++) {
-        int num=i;
-        int array[50];
-        int k=0;
-        while (num>0) {
-            array[k]=num%2;
-            num=num/2;
-            k++;
-        }
-        for (int j=k-1; j>=0; j--) {
-            cout<<array[j];
-        }
-        cout<<"\n";
-    }
 }
