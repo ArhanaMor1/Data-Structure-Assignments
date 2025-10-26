9e04085856ffcdc402b19523a8fe91628bf3719e
ArhanaMor1 <amor_be24@thapar.edu>
2025-10-26 23:54:09 +0530

    Update Additional_ques4.cpp

--- Assignment6/Additional_ques4.cpp
+++ Assignment6/Additional_ques4.cpp
@@ -9,14 +9,19 @@ struct Node {
 
 Node* head = NULL;
 
-void append(int x) {
-    Node* n = new Node{x, NULL, NULL};
-    if(head == NULL) {
+void add_at_end(int x) {
+    Node* n = new Node;
+    n->data = x;
+    n->next = NULL;
+    n->prev = NULL;
+
+    if (head == NULL) {
         head = n;
         return;
     }
+
     Node* temp = head;
-    while(temp->next != NULL)
+    while (temp->next != NULL)
         temp = temp->next;
     temp->next = n;
     n->prev = temp;
@@ -24,30 +29,30 @@ void append(int x) {
 
 void display() {
     Node* temp = head;
-    while(temp != NULL) {
+    while (temp != NULL) {
         cout << temp->data << " -> ";
         temp = temp->next;
     }
     cout << "NULL\n";
 }
 
-void displayReverse() {
-    if(head == NULL) return;
+void display_reverse() {
+    if (head == NULL) return;
     Node* temp = head;
-    while(temp->next != NULL)
+    while (temp->next != NULL)
         temp = temp->next;
-    while(temp != NULL) {
+    while (temp != NULL) {
         cout << temp->data << " -> ";
         temp = temp->prev;
     }
     cout << "NULL\n";
 }
 
-void correctPointers() {
-    if(head == NULL) return;
+void correct_pointer() {
+    if (head == NULL) return;
     Node* temp = head;
-    while(temp->next != NULL) {
-        if(temp->next->prev != temp)
+    while (temp->next != NULL) {
+        if (temp->next->prev != temp)
             temp->next->prev = temp;
         temp = temp->next;
     }
@@ -56,19 +61,17 @@ void correctPointers() {
 int main() {
     head = new Node{1, NULL, NULL};
     head->next = new Node{2, NULL, head};
-    head->next->next = new Node{3, NULL, head}; // wrong prev
+    head->next->next = new Node{3, NULL, head}; 
     head->next->next->next = new Node{4, NULL, head->next->next};
     head->next->next->next->prev = head->next->next;
 
-    cout << "Incorrect List (Forward): ";
+    cout << "Incorrect List:\n";
     display();
-    cout << "Incorrect List (Reverse): ";
-    displayReverse();
+    display_reverse();
 
-    correctPointers();
+    correct_pointer();
 
-    cout << "\nCorrected List (Forward): ";
+    cout << "\nCorrected List:\n";
     display();
-    cout << "Corrected List (Reverse): ";
-    displayReverse();
+    display_reverse();
 }
